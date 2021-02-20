f931143e66fc64138ed27abd2bd0e96cadfb59d9
Matt Zimmerman <mdz@alcor.net>
2021-02-20 15:38:28 -0800

    turn __main__ into a proper CLI

--- smarttub/__main__.py
+++ smarttub/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import datetime
 import logging
@@ -6,47 +7,106 @@ import sys
 
 import aiohttp
 
-from . import SmartTub
-
-logging.basicConfig(level=logging.DEBUG)
+from . import SmartTub, SpaLight
 
 
-async def main(args):
+async def info_command(spas, args):
+    for spa in spas:
+        print(f"= Spa '{spa.name}' =\n")
+        if args.all or args.status:
+            print("== Status ==")
+            pprint(await spa.get_status())
+            print()
+
+        if args.all or args.pumps:
+            print("== Pumps ==")
+            for pump in await spa.get_pumps():
+                print(pump)
+            print()
+
+        if args.all or args.lights:
+            print("== Lights ==")
+            for light in await spa.get_lights():
+                print(light)
+            print()
+
+        if args.all or args.errors:
+            print("== Errors ==")
+            for error in await spa.get_errors():
+                print(error)
+            print()
+
+        if args.all or args.reminders:
+            print("== Reminders ==")
+            for reminder in await spa.get_reminders():
+                print(reminder)
+            print()
+
+        if args.all or args.energy:
+            print("== Energy usage ==")
+            energy_usage_day = spa.get_energy_usage(spa.EnergyUsageInterval.DAY, end_date=datetime.date.today(), start_date=datetime.date.today() - datetime.timedelta(days=7))
+            pprint(await energy_usage_day)
+            print()
+
+        if args.all or args.debug:
+            debug_status = await spa.get_debug_status()
+            print(f"== Debug status ==\n{debug_status}\n")
+
+        print()
+
+
+async def set_command(spas, args):
+    for spa in spas:
+        if args.temperature:
+            await spa.set_temperature(38.3)
+
+        if args.light_mode:
+            for light in await spa.get_lights():
+                await light.set_mode(light.LightModes[args.lights], 25)
+                if args.verbosity > 0:
+                    print(light)
+
+
+async def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", required=True)
+    parser.add_argument("-p", "--password", required=True)
+    parser.add_argument("-v", "--verbosity", action="count", default=0)
+
+    subparsers = parser.add_subparsers()
+    info_parser = subparsers.add_parser("info")
+    info_parser.set_defaults(func=info_command)
+    info_parser.add_argument("-a", "--all", action="store_true")
+    info_parser.add_argument("--spas", action="store_true")
+    info_parser.add_argument("--status", action="store_true")
+    info_parser.add_argument("--pumps", action="store_true")
+    info_parser.add_argument("--lights", action="store_true")
+    info_parser.add_argument("--errors", action="store_true")
+    info_parser.add_argument("--reminders", action="store_true")
+    info_parser.add_argument("--debug", action="store_true")
+    info_parser.add_argument("--energy", action="store_true")
+
+    set_parser = subparsers.add_parser("set")
+    set_parser.set_defaults(func=set_command)
+    set_parser.add_argument("-l", "--light_mode", choices=[mode.name for mode in SpaLight.LightMode])
+    set_parser.add_argument("-t", "--temperature", type=float)
+
+    args = parser.parse_args(argv)
+
+    if args.verbosity > 1:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    logging.basicConfig(level=log_level)
+
     async with aiohttp.ClientSession() as session:
         st = SmartTub(session)
-        await st.login(*args[0:2])
+        await st.login(args.username, args.password)
+
         account = await st.get_account()
-        print(account)
+
         spas = await account.get_spas()
-        print(spas)
-        for spa in spas:
-            status = spa.get_status()
-            pumps = spa.get_pumps()
-            lights = spa.get_lights()
-            errors = spa.get_errors()
-            reminders = spa.get_reminders()
-
-            pprint(await status)
-            for pump in await pumps:
-                print(pump)
-            for light in await lights:
-                print(light)
-            for reminder in await reminders:
-                print(reminder)
-            pprint(await errors)
-
-            if len(args) > 2:
-                await light.set_mode(light.LightMode.RED, 25)
-                lights = await spa.get_lights()
-                print(lights[0])
-
-        if len(args) > 3:
-            debug_status = spa.get_debug_status()
-            energy_usage_day = spa.get_energy_usage(spa.EnergyUsageInterval.DAY, end_date=datetime.date.today(), start_date=datetime.date.today() - datetime.timedelta(days=7))
-            set_temp = spa.set_temperature(38.3)
-            await set_temp
-            pprint(await energy_usage_day)
-            pprint(await debug_status)
-            # await st._refresh_token()
+        await args.func(spas, args)
 
 asyncio.run(main(sys.argv[1:]))
