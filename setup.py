2b8e38922209107f72efaa8d165bc54ec8b92bbc
Matt Zimmerman <mdz@alcor.net>
2021-03-14 13:53:14 -0700

    list package instead of find_packages, release 0.0.22

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="python-smarttub",
-    version="0.0.21",
+    version="0.0.22",
     author="Matt Zimmerman",
     author_email="mdz@alcor.net",
     description="API to query and control hot tubs using the SmartTub system",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mdz/python-smarttub",
-    packages=setuptools.find_packages(),
+    packages=["smarttub"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
